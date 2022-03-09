58c2deabb92208536a70f088c64caf6bfae10bb4
Anders Christensen <89982133+McAndersC@users.noreply.github.com>
2022-03-09 15:39:20 +0100

    Step 04 Client Boiler Plate

---
+++ step_04_client_boilerplate/lib/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const path = require('path');
+
+const server = {};
+
+const expressServer = express();
+
+// Express server middleware
+expressServer.use(express.static('src'));
+
+expressServer.get('/', (req, res) => {
+
+    console.log(__dirname)
+    res.sendFile(path.join(__dirname, '../index.html'));
+
+});
+
+server.run = () => {
+
+    let port = 3002;
+
+    expressServer.listen(port, () => {
+
+        console.log('\x1b[32m%s\x1b[0m','Serveren benytter følgende adresse http://localhost:' + port);
+
+    })
+
+}
+
+
+module.exports = server;
+
