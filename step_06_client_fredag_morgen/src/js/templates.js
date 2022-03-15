1ec1f5b470c0e8c3145b50c5c730abd129fd2c1d
Anders Christensen <89982133+McAndersC@users.noreply.github.com>
2022-03-15 14:16:29 +0100

    Remove all steps. Leaving final

--- step_06_client_fredag_morgen/src/js/templates.js
+++
@@ -1,9 +0,0 @@
-export const emptyTemplate2 = `<span>-</span>
-    <span>-</span>
-    <span>-</span>
-    <span>-</span>`;
-
-export const userTemplate2 = (user) => `<span>${user._id}</span>
-    <span>${user.profile}</span>
-    <span>${user.username}</span>
-    <span>-</span>`
